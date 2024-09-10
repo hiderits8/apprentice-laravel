a32249268887fdde1f996fb4890068896d716db0
hiderits8 <wataru.ts8.hideri@gmail.com>
2024-09-10 22:11:36 +0900

    first commit

---
+++ database/migrations/2024_09_10_212724_create_articles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->integer('fav');
+            $table->string('title');
+            $table->string('lead');
+            $table->string('text');
+            $table->string('tag');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
---
+++ database/seeders/ArticleSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ArticleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(20),
+            'fav' => $this->faker->numberBetween(0, 500),
+            'title' => $this->faker->realText(50),
+            'lead' => $this->faker->realText(100),
+            'text' => $this->faker->realText(1000),
+        ];
+    }
+}
